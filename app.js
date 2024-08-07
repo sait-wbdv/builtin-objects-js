ce31cbac9c9c90ace680a1318ba9c87f722f8de9
Ashlyn Knox <ashlyn@lilyx.io>
2024-08-07 16:34:40 -0600

    starter code and instructions

---
+++ app.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// Date object
+const today = new Date();
+
+// logging the whole date object
+console.log(`Todays date is: ${today}`);
+
+// 1. Log Month Day Year
+
+// 2. Log the Day of the week: Use the array + date object together
+
+const dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+// 3. Update the variable to it gives tomorrows date and log it
+
+let tomorrow = new Date();
+
+// Math Object
+
+const piValue = Math.PI;
+
+console.log(`The value of pi: ${piValue}`);
+
+// 1. Get the square root of 25
+
+// 2. Round 3.75
+
+// 3. Round 22.85 down
+
+// 4. Find the largest number between 5, 12, and 8
+
+// 5. Generate a random number between 0 and 1
+
+// Bonus. Generate a random number between 1 and 100
